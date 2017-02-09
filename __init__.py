1cd1f5f67dd5324e73a341f756793d9789b95a26
Théo Friberg <theo.friberg@gmail.com>
2017-02-09 21:09:05 +0200

    Basis for VSE integration

--- __init__.py
+++ __init__.py
@@ -35,7 +35,7 @@ if "bpy" in locals():
         import importlib
 
         try:
-            modules = (node_tree, piano_capture, oscillators, ugen_operators, effects)
+            modules = (node_tree, piano_capture, oscillators, ugen_operators, effects, vse_operators)
             for m in modules:
                 importlib.reload(m)
             print("audio_nodes: reloaded modules, all systems operational")
@@ -47,7 +47,7 @@ if "bpy" in locals():
 
 import bpy
 
-from . import node_tree, piano_capture, oscillators, ugen_operators, effects
+from . import node_tree, piano_capture, oscillators, ugen_operators, effects, vse_operators
 
 
 ### Node Categories ###
