3839b6e2c3935b4e58a4bd155b0250def51a9311
Théo Friberg <Tho Friberg>
2017-12-31 18:01:57 +0200

    Added --debug flag

--- compile.sh
+++ compile.sh
@@ -2,4 +2,9 @@
 
 # Dependencies: libsdl2-dev
 
-g++ -O2 --std=c++14 -Wall -pedantic $@ -o native.so -fPIC -shared native/*.cpp -lSDL2
+if [ $1 == "--debug" ]
+then
+  g++ -O2 --std=c++14 -Wall -pedantic $@ -o native.so -fPIC -shared native/*.cpp -lSDL2 -g
+else
+  g++ -O2 --std=c++14 -Wall -pedantic $@ -o native.so -fPIC -shared native/*.cpp -lSDL2 -march=native
+fi
