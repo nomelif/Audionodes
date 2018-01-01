521ff697db8893435a9f5264580be60c11949c85
Théo Friberg <Tho Friberg>
2018-01-01 18:12:17 +0200

    Added dependency on fluidsynth

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 
 if [ "$1" == "--debug" ]
 then
-  g++ -O2 --std=c++14 -Wall -pedantic $@ -o native.so -fPIC -shared native/*.cpp -lSDL2 -g
+  g++ -O2 --std=c++14 -Wall -pedantic $@ -o native.so -fPIC -shared native/*.cpp -lSDL2 -lfluidsynth -g
 else
-  g++ -O2 --std=c++14 -Wall -pedantic $@ -o native.so -fPIC -shared native/*.cpp -lSDL2 -march=native
+  g++ -O2 --std=c++14 -Wall -pedantic $@ -o native.so -fPIC -shared native/*.cpp -lSDL2 -lfluidsynth  -march=native
 fi
