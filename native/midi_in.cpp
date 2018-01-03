92a340506df4a5b67ec7681ec6cf91c704c79bab
Roope Salmi <rpsalmi@gmail.com>
2018-01-04 01:33:51 +0200

    Attempt to correct midi timing wobble
    This doesn't work in all scenarios... We probably need a better scheduler.

--- native/midi_in.cpp
+++ native/midi_in.cpp
@@ -1,6 +1,7 @@
 #include "midi_in.hpp"
 
 
+#include <iostream>
 int MidiIn::handle_midi_event(void* _node, fluid_midi_event_t* event){
   using namespace std::chrono;
   MidiIn *node = (MidiIn*)_node;
@@ -15,9 +16,21 @@ int MidiIn::handle_midi_event(void* _node, fluid_midi_event_t* event){
   );
   std::lock_guard<std::mutex> lock(node->event_buffer_mutex);
   Clock::time_point time = Clock::now();
-  our_event.time = duration_cast<SampleDuration>(time - node->last_process).count();
+  our_event.time = (
+    duration_cast<SampleDuration>(time - node->last_process)
+    + node->timing_offset
+  ).count();
+  bool to_next = false;
+  if (our_event.time >= N) {
+    our_event.time -= N;
+    to_next = true;
+  }
   our_event.time = std::min(std::max(our_event.time, size_t(0)), N-1);
-  node->event_buffer.push_back(our_event);
+  if (!to_next) {
+    node->event_buffer.push_back(our_event);
+  } else {
+    node->event_buffer_to_next.push_back(our_event);
+  }
   return 0;
 }
 
@@ -39,8 +52,16 @@ NodeOutputWindow MidiIn::process(NodeInputWindow &input) {
   MidiData::EventSeries events;
   { std::lock_guard<std::mutex> lock(event_buffer_mutex);
     events = std::move(event_buffer);
-    event_buffer = MidiData::EventSeries();
-    last_process = Clock::now();
+    event_buffer = std::move(event_buffer_to_next);
+    event_buffer_to_next = MidiData::EventSeries();
+    Clock::time_point this_process = Clock::now();
+    timing_offset = std::chrono::duration_cast<SampleDuration>(this_process-last_process);
+    if (timing_offset < SampleDuration(N)) timing_offset = SampleDuration(0);
+    else timing_offset -= SampleDuration(N);
+    last_process = this_process;
+  }
+  for (auto &p : events) {
+    std::cout << p.time << std::endl;
   }
   return NodeOutputWindow({new MidiData(events)});
 }
--- native/midi_in.hpp
+++ native/midi_in.hpp
@@ -14,8 +14,9 @@ class MidiIn : public Node {
   typedef std::chrono::duration<size_t, std::ratio<1, rate>> SampleDuration;
   fluid_midi_driver_t* driver;
   static int handle_midi_event(void* data, fluid_midi_event_t* event);
-  MidiData::EventSeries event_buffer;
+  MidiData::EventSeries event_buffer, event_buffer_to_next;
   std::mutex event_buffer_mutex;
+  SampleDuration timing_offset;
   Clock::time_point last_process;
   public:
   MidiIn();
