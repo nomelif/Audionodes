d5e5447383b4a6a55da61ccbcbc824ca65a85db7
Roope Salmi <rpsalmi@gmail.com>
2018-01-06 01:40:49 +0200

    And those commented lines -- yeah we won't need them any more

--- native/midi_in.hpp
+++ native/midi_in.hpp
@@ -10,8 +10,6 @@
 #include "circular_buffer.hpp"
 
 class MidiIn : public Node {
-  // typedef std::chrono::steady_clock Clock;
-  // typedef std::chrono::duration<size_t, std::ratio<1, rate>> SampleDuration;
   fluid_midi_driver_t* driver;
   static int handle_midi_event(void* data, fluid_midi_event_t* event);
   CircularBuffer<MidiData::Event, 1024> event_buffer;
