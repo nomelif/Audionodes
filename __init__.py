0769b2202fff6ed40aa3fc72f23b3c96b11445ed
Roope Salmi <rpsalmi@gmail.com>
2020-10-09 01:59:44 +0300

    Bump version

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "name": "Audionodes",
     "description": "Create complex soundscapes in real time using nodes.",
     "author": "Roope Salmi, Théo Friberg",
-    "version": (0,3,2),
+    "version": (0,3,3),
     "blender": (2,80,0),
     "location": "Node Editor > Sound Icon > Add new",
     "warning": "Very much alpha, may blow up in your face.",
